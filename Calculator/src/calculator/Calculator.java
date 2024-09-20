37b974f6101527b9f2f8c35b8cf011465df09a15
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-09-20 10:49:20 +0200

    TP0

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -18,10 +18,28 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int operateur;
+        float operande1;
+        float operande2;
+    
         Scanner sc;
         sc = new Scanner(System.in);
         System.out.println("Please enter the operator \n1) add\n2) substract\n3) multiply\n4) divide\n5)modulo");
-        operation = sc.nextInt();
+        operateur = sc.nextInt();
+        System.out.println("Saisissez votre premier nombre.");
+        operande1 = sc.nextFloat();
+        System.out.println("Saisissez votre second nombre.");
+        operande2 = sc.nextFloat();
+        if(operateur == 1)
+            System.out.println("Le resultat est "+(operande1+operande2));
+        if(operateur == 2)
+            System.out.println("Le resultat est "+(operande1-operande2));
+        if(operateur == 3)
+            System.out.println("Le resultat est "+(operande1*operande2));
+        if(operateur == 4)
+            System.out.println("Le resultat est "+(operande1/operande2));
+        if(operateur == 5)
+            System.out.println("Le resultat est "+(operande1%operande2));
         
     }       
 }
