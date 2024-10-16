6498b310a001bcf0b7b90c1203c0012ac161fcd0
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-10-16 08:54:52 +0200

    TP2 exercice 3

---
+++ TP2_manip_REMY/src/tp2_manip_remy/Tartiflette.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_remy;
+
+/**
+ *
+ * @author Maël
+ */
+public class Tartiflette {
+
+    int nbCalories = 0;
+    int calories;
+
+    public Tartiflette(int calories) {
+        nbCalories += calories;
+
+    }
+    public void Attribution(int calories){
+        nbCalories += calories;
+    }
+
+
+}
