7efbba3a686e8b5ae61f75afdfd2187babe6ac77
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-11-13 18:49:27 +0100

    TP3 arme

---
+++ TP3_Heroic_Fantasy_REMY/src/tp3_heroic_fantasy_remy/Arme.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_remy;
+
+/**
+ *
+ * @author Maël
+ */
+public abstract class Arme {
+
+    String nom;
+    private int attaque;
+
+    public Arme(String nom, int attaque) {
+        this.nom = nom;
+        if (attaque > 100) {
+            this.attaque = 0;
+            System.out.println("L'attaque de votre arme est devenu nulle sale merde");
+        } else {
+            this.attaque = attaque;
+        }
+    }
+
+    public void SetAttaque(int attaque) {
+        if (attaque > 100) {
+            this.attaque = 0;
+            System.out.println("L'attaque de votre arme est devenu nulle sale merde");
+        } else {
+            this.attaque = attaque;
+        }
+    }
+
+    public int GetAttaque() {
+        return this.attaque;
+    }
+
+    public String GetNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Arme{" + "nom=" + nom + ", attaque=" + attaque + '}';
+    }
+
+}
