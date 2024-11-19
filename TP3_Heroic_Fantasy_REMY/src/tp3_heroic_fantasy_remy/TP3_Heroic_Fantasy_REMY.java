469649626a6bf1b58960f3a8c4d57a14a08beff7
Maël REMY--GENIN <mael.remy--genin@epfedu.fr>
2024-11-19 23:05:54 +0100

    TP3 code principal

--- TP3_Heroic_Fantasy_REMY/src/tp3_heroic_fantasy_remy/TP3_Heroic_Fantasy_REMY.java
+++ TP3_Heroic_Fantasy_REMY/src/tp3_heroic_fantasy_remy/TP3_Heroic_Fantasy_REMY.java
@@ -6,6 +6,7 @@ package tp3_heroic_fantasy_remy;
 
 import Armes.*;
 import java.util.ArrayList;
+import Personages.*;
 
 /**
  *
@@ -30,13 +31,34 @@ public class TP3_Heroic_Fantasy_REMY {
         stuff.add(Charme);
         for (int i = 0; i <= stuff.size() - 1; i++) {
             System.out.print("L'arme " + stuff.get(i).GetNom() + " a " + stuff.get(i).GetAttaque() + " d'attaque et ");
-            
-            if (stuff.get(i) instanceof Baton){
-            System.out.println(((Baton) stuff.get(i)).getAge()+ " d'age.");
-            }
-            if (stuff.get(i) instanceof Epee){
-            System.out.println(((Epee) stuff.get(i)).getFinesse()+ " de finesse.");
 
+            if (stuff.get(i) instanceof Baton) {
+                System.out.println(((Baton) stuff.get(i)).getAge() + " d'age.");
+            }
+            if (stuff.get(i) instanceof Epee) {
+                System.out.println(((Epee) stuff.get(i)).getFinesse() + " de finesse.");
+            }
+        }
+
+        Personnage Gandalf = new Magicien(true, "Gandalf", 65);
+        Personnage Garcimore = new Magicien(false, "Garcimore", 44);
+        Personnage Conan = new Guerrier(false, "Conan", 78);
+        Personnage Lannister = new Guerrier(true, "Lannister", 45);
+
+        ArrayList<Personnage> liste = new ArrayList<Personnage>();
+        liste.add(Gandalf);
+        liste.add(Garcimore);
+        liste.add(Conan);
+        liste.add(Lannister);
+
+        for (int i = 0; i <= liste.size() - 1; i++) {
+            System.out.print("Le heros " + liste.get(i).GetNom() + " a " + liste.get(i).GetPV() + " pv et ");
+
+            if (liste.get(i) instanceof Magicien) {
+                System.out.println(((Magicien) liste.get(i)).Confirme());
+            }
+            if (liste.get(i) instanceof Guerrier) {
+                System.out.println(((Guerrier) liste.get(i)).Cheval());
             }
         }
     }
